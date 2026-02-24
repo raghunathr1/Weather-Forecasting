3a8eef6f1470e45d1e2f6afa0e33600cca99bcba
raghunathr1 <raghunathkapase2004@gmail.com>
2026-02-24 13:04:11 +0430

    Add event listeners for weather search functionality

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 const API_KEY = "485f9bc87c18cb5fc1bb5f1d37d19882";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const locationBtn = document.getElementById("locationBtn");
+//  STATE
+
+let currentTempCelsius = null;
+let isCelsius = true;
+
+// EVENT LISTENERS
+
+searchBtn.addEventListener("click", () => {
+  const city = cityInput.value.trim();
+  if (!city) {
+    showError("Please enter a city name.");
+    return;
+  }
+  fetchWeatherByCity(city);
+});
+
+locationBtn.addEventListener("click", getWeatherByLocation);
+
+recentCitiesSelect.addEventListener("change", () => {
+  const city = recentCitiesSelect.value;
+  if (city) {
+    fetchWeatherByCity(city);
+  }
+});
+
+unitToggleBtn.addEventListener("click", toggleTemperatureUnit);
