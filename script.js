5099e67787713164f60c2a051b040176db1e6340
raghunathr1 <raghunathkapase2004@gmail.com>
2026-02-24 13:09:47 +0430

    Add error handling and weather fetching functions

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 const locationBtn = document.getElementById("locationBtn");
+const errorMessage = document.getElementById("errorMessage");
+const weatherIcon = document.getElementById("weatherIcon");
+
 //  STATE
 
 let currentTempCelsius = null;
@@ -30,3 +33,72 @@ recentCitiesSelect.addEventListener("change", () => {
 });
 
 unitToggleBtn.addEventListener("click", toggleTemperatureUnit);
+// FETCH FUNCTIONS
+
+async function fetchWeatherByCity(city) {
+  clearError();
+  try {
+    const currentRes = await fetch(
+      `${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`
+    );
+
+    if (!currentRes.ok) {
+      throw new Error("City not found");
+    }
+
+    const currentData = await currentRes.json();
+    displayCurrentWeather(currentData);
+    saveRecentCity(currentData.name);
+
+    fetchForecast(currentData.coord.lat, currentData.coord.lon);
+  } catch (error) {
+    showError(error.message);
+  }
+}
+
+function getWeatherByLocation() {
+  if (!navigator.geolocation) {
+    showError("location is not supported by your browser.");
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      fetchWeatherByCoords(latitude, longitude);
+    },
+    () => {
+      showError("Unable to retrieve your location.");
+    }
+  );
+}
+
+async function fetchWeatherByCoords(lat, lon) {
+  clearError();
+  try {
+    const currentRes = await fetch(
+      `${BASE_URL}/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+    );
+
+    const currentData = await currentRes.json();
+    displayCurrentWeather(currentData);
+    saveRecentCity(currentData.name);
+
+    fetchForecast(lat, lon);
+  } catch {
+    showError("Failed to fetch weather data.");
+  }
+}
+
+async function fetchForecast(lat, lon) {
+  try {
+    const forecastRes = await fetch(
+      `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+    );
+
+    const forecastData = await forecastRes.json();
+    displayForecast(forecastData.list);
+  } catch {
+    showError("Unable to fetch forecast data.");
+  }
+}
